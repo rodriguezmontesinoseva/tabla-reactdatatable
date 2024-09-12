5c6a875fadd0957a94813cbb4608156f48252ac7
Eva Rodriguez <evarmon@ext.inditex.com>
2024-09-12 15:36:25 +0200

    added React Data Table basic

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import DataTable from 'react-data-table-component'
+import { useEffect, useState } from 'react'
+import { data } from './constants/data'
+import { columns } from './constants/columns'
+
+function App() {
+  const [filteredData, setFilteredData] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFilteredData(data)
+      setLoading(false)
+    }, 2000)
+    return () => clearTimeout(timeout)
+  }, [])
+
+  const handleChange = e => {
+    console.log('e ', e.target.value)
+    // FILTRADO POR UNA CLAVE
+    //  const newData = data.filter(data => data.titulo.includes(e.target.value))
+
+    // FILTRADO POR TODAS LAS CLAVES
+    const newData = data.filter(item =>
+      Object.values(item).some(value =>
+        value.toString().toLowerCase().includes(e.target.value.toLowerCase()),
+      ),
+    )
+    setFilteredData(newData)
+  }
+
+  const Loading = () => {
+    return (
+      <div>
+        <h1>Cargando los datos</h1>
+        <h3>spinner</h3>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={handleChange} />
+      <DataTable
+        title="Biblioteca"
+        columns={columns}
+        data={filteredData}
+        selectableRows
+        onSelectedRowsChange={data => console.log(data)}
+        pagination
+        paginationPerPage={6}
+        fixedHeader
+        progressPending={loading} // muestra el texto ...loading
+        progressComponent={<Loading />} // muestra spinner personalizado
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ src/constants/columns.js
@@ -0,0 +1,8 @@
+export const columns = [
+  { name: "Título", selector: row => row.titulo, sortable: true },
+  { name: "Autor", selector: row => row.autor, sortable: true },
+  { name: "Año", selector: row => row.año },
+  { name: "Género", selector: row => row.genero },
+  { name: "Páginas", selector: row => row.paginas },
+  { name: "ISBN", selector: row => row.isbn },
+]
